531ced35a5c996f8f1b76b1f34cf965ebe7d58b9
Jos Bonsink <josbonsink@gmail.com>
2013-12-02 22:29:37 +0100

    Unity

---
+++ Cargo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	public int x = 5, y = 5;
+	public GUIStyle button; 
+	public GUISkin mySkin;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		GUI.skin = mySkin;
+		if(GUI.Button(new Rect(x,y,150,100), "Start game"))
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
