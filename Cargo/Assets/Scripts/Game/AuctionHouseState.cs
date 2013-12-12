38b53879d4cbe0227c7035ed73af0f3380a853ee
Joshua Bonsink <josbonsink@gmail.com>
2013-12-12 19:35:32 +0100

    auction house overlay

---
+++ Cargo/Assets/Scripts/Game/AuctionHouseState.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AuctionHouseState : State 
+{
+	private int width, height;
+	private State returnToState;
+	private string buyCaption = "Buy";
+	private string sellCaption = "Sell";
+	private string quantityCaption = "Quantity";
+	private string auctionHouseCaption = "Auction house";
+	private string buttonStyle = "hudButton";
+	
+	private bool returnToPrevState = false;
+	
+	public AuctionHouseState(State returnToState)
+	{
+		width = Screen.width;
+		height = Screen.height;
+		this.returnToState = returnToState;
+	}
+	
+	public State UpdateState()
+	{
+		if (returnToPrevState)
+		{
+			returnToPrevState = false;
+			return returnToState;
+		}
+		
+		GUI.Window(0, new Rect(width / 2 - 100, height - 200, 200, 100), AuctionHouseWindow, auctionHouseCaption);
+		
+		return this;
+	}
+	
+	public void AuctionHouseWindow(int ID)
+	{
+		GUILayout.BeginArea(new Rect(0, 0, 200, 40));
+		{
+			if(GUILayout.Button("Back", "hudButton")) returnToPrevState = true;
+		}
+		
+		GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+	}
+}
--- Cargo/Assets/Scripts/Game/NavigationState.cs
+++ Cargo/Assets/Scripts/Game/NavigationState.cs
@@ -3,17 +3,29 @@ using System.Collections;
 
 public class NavigationState : State
 {
+	int width, height;
 	private Ship ship;
 	private Vector3 cursorPos = new Vector3();
-
-
+	private AuctionHouseState auctionHouseState; 
+	private string inventoryCaption = "Inventory";	
+	private string buttonStyle = "hudButton";
+	
 	public NavigationState(Ship ship)
 	{
+		width = Screen.width;
+		height = Screen.height;
 		this.ship = ship;
+		auctionHouseState = new AuctionHouseState(this);
 	}
 
 	public State UpdateState()
 	{
+		GUI.BeginGroup(new Rect(0, 0, width, 100));
+		{
+			if(GUI.Button(new Rect(0, 0, 200, 40), inventoryCaption, buttonStyle)) return auctionHouseState;
+		}
+		GUI.EndGroup();
+		
 		cursorPos.Set(Input.mousePosition.x, Input.mousePosition.y, 0);
 		cursorPos = Camera.main.ScreenToWorldPoint(cursorPos);
 		
