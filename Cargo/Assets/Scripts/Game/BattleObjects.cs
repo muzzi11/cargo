2e5b2858216187521452b9f207904eb674a888f6
Muzzi11 <muzzi11@hotmail.com>
2013-12-20 20:28:16 +0100

    3d sounds are now mono, removed debug logging for lasers

--- Cargo/Assets/Scripts/Game/BattleObjects.cs
+++ Cargo/Assets/Scripts/Game/BattleObjects.cs
@@ -62,7 +62,6 @@ public class BattleObjects : MonoBehaviour
 	{
 		var laserbeam = Instantiate(laserbeamPrefab) as Laserbeam;
 		laserbeam.SetRay(start, end);
-		Debug.Log (laserbeam);
 		PlayLaser();
 	}
 
