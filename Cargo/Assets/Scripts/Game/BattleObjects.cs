6fa9760060e38c2a326927c71bd7f4f65de6d096
Muzzi11 <muzzi11@hotmail.com>
2013-12-20 01:26:25 +0100

    BattleState has working battles now, still prototype

---
+++ Cargo/Assets/Scripts/Game/BattleObjects.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleObjects : MonoBehaviour
+{
+	public GameObject shipPrefab, damageTextPrefab;
+	public Sprite[] shipSprites;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public GameObject InstantiateShip(int shipIndex, Vector2 position)
+	{
+		if(shipIndex < 0 || shipIndex > shipSprites.Length) Debug.LogError("Ship index out of bounds");
+
+		var ship = Instantiate(shipPrefab, position, Quaternion.identity) as GameObject;
+		ship.GetComponent<SpriteRenderer>().sprite = shipSprites[shipIndex];
+
+		return ship;
+	}
+
+	public void InstantiateDamageText(string text, Vector2 position)
+	{
+		var damageText = Instantiate(damageTextPrefab, position, Quaternion.identity) as GameObject;
+		damageText.guiText.text = text;
+	}
+}
---
+++ Cargo/Assets/Scripts/Game/DamageText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageText : MonoBehaviour
+{
+	public float scroll = 0.05f;
+	public float duration = 1.5f;
+	private float startTime;
+	
+	void Start()
+	{
+		startTime = Time.time;
+	}
+	
+	void Update()
+	{
+		float dt = Time.time - startTime;
+
+		if(dt < duration)
+		{
+			var position = transform.position;
+			var color = guiText.material.color;
+
+			position.y += scroll * Time.deltaTime;
+			transform.position = position;
+
+			float alpha = dt / duration;
+			alpha = 1.0f - alpha * alpha * alpha;
+			color.a = alpha;
+			guiText.material.color = color;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
